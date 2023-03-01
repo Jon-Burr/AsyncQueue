7b0d948d7803ce976ea27966bd9ec8635881eae3
Jon Burr <jon.burr@cern.ch>
2023-03-01 15:31:22 +0000

    Fix consumers

--- include/AsyncQueue/MessageSource.h
+++ include/AsyncQueue/MessageSource.h
@@ -4,6 +4,8 @@
 #include "AsyncQueue/Message.h"
 #include "AsyncQueue/MessageSource.h"
 
+#include <vector>
+
 namespace AsyncQueue {
 
     /**
@@ -43,9 +45,12 @@ namespace AsyncQueue {
         MessageBuilder &operator<<(stream_mod_t mod) {
             if (m_void)
                 return *this;
-            if (mod == &std::endl<std::ostream::char_type, std::ostream::traits_type>)
-                flush();
-            else
+            if (mod == &std::endl<std::ostream::char_type, std::ostream::traits_type>) {
+                m_messages.push_back(m_msg.str());
+                // Clear the message buffer
+                m_msg.str("");
+                m_empty = true;
+            } else
                 // OK - this was actually a generic stream modifier, not endl
                 m_msg << mod;
             return *this;
@@ -59,6 +64,7 @@ namespace AsyncQueue {
         // The message level
         const MessageLevel m_lvl;
         std::ostringstream m_msg;
+        std::vector<std::string> m_messages;
 
     }; //> end class MessageBuilder
 
--- include/AsyncQueue/TeeConsumer.h
+++ include/AsyncQueue/TeeConsumer.h
@@ -10,7 +10,7 @@
 namespace AsyncQueue {
     /// @brief Consumer that takes an element and forwards it to multiple other consumers
     /// @tparam T The element type of the queue
-    template <typename T> class TeeConsumer : IConsumer<T> {
+    template <typename T> class TeeConsumer : public IConsumer<T> {
     public:
         /// @brief Create a tee consumer with an empty list of children
         TeeConsumer() = default;
@@ -21,6 +21,7 @@ namespace AsyncQueue {
         /// @brief Add a consumer to this
         /// @param consumer
         void addConsumer(std::unique_ptr<IConsumer<T>> &&consumer);
+
         /// @brief Add a consumer to this
         /// @tparam C The consumer type
         /// @param consumer
@@ -29,14 +30,17 @@ namespace AsyncQueue {
                 std::is_base_of_v<IConsumer<T>, C> && std::is_move_constructible_v<C>, void>
         addConsumer(C &&consumer);
 
+        void addConsumer(IConsumer<T> *consumer);
+
         TaskStatus consume(const T &element) override;
 
     private:
-        std::vector<std::unique_ptr<IConsumer<T>>> m_consumers;
+        std::vector<std::unique_ptr<IConsumer<T>>> m_ownedConsumers;
+        std::vector<IConsumer<T> *> m_consumers;
 
     }; //> end class TeeConsumer<T>
 } // namespace AsyncQueue
 
-#include "AsyncQueue/IConsumer.icc"
+#include "AsyncQueue/TeeConsumer.icc"
 
 #endif //> !ASYNCQUEUE_TEECONSUMER_H
